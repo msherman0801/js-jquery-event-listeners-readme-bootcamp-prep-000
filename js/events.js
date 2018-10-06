fef6bdef48dc3f798a81dab77732f89210b3f335
Matthew Sherman <msherman0801@gmail.com>
2018-10-06 16:48:54 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
 getIt();
 frameIt();
-submitIt();
+pressIt();
 });
 
 function getIt() {
@@ -19,7 +19,7 @@ function frameIt() {
   })
 }
 
-function submitIt() {
+function pressIt() {
   $('input:first').on('keypress', function(e) {
     if(e.which == 71) {
       alert("You have pressed the G key")
