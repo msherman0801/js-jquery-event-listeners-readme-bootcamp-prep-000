d33252467f239e3953941fa397cd143577ead804
Matthew Sherman <msherman0801@gmail.com>
2018-10-06 16:51:22 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -2,6 +2,33 @@
 
 $(document).ready(function(){
 
-// call functions here
-
+getIt();
+frameIt();
+pressIt();
 });
+
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!')
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  })
+}
+
+function pressIt() {
+  $('input:first').bind('keydown', function(e) {
+    if(e.which == 71) {
+      alert("You have pressed the G key")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').bind('submit', function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
